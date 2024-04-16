a5e82fe06e4c405a545b2622731990213a735d30
Ahmed Hussein <Ahmed-Hussein-ELShahat@gmail.com>
2024-04-16 15:12:37 +0200

    Adding getters and setters

--- Resturant managment system/src/Backend/Order.java
+++ Resturant managment system/src/Backend/Order.java
@@ -1,6 +1,37 @@
 package Backend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Order {
-    Item[]x;
-    static int TotalNumberOfOrders;
+    private ArrayList<Item> guestOrder;             // Guest's order
+    private static int TotalNumberOfOrders = 0;     // number of orders by all guests
+
+    public Order(Item[] guestOrder){
+        TotalNumberOfOrders++;
+        this.guestOrder = new ArrayList<>(Arrays.asList(guestOrder));
+    }
+
+    public Order(){
+        TotalNumberOfOrders++;
+    }
+
+    public Item getOrder(int n){return guestOrder.get(n);}          //return 1 specific item only
+
+    public Item[] getOrder(){return new Item[guestOrder.size()];}   //return the total order in the shape of an array
+
+    public static int getTotalNumberOfOrders() {return TotalNumberOfOrders;}
+
+    public int getNumOfItems(){return guestOrder.size();}
+
+    public void setGuestOrder(ArrayList<Item> guestOrder){this.guestOrder = guestOrder;}
+
+    public void setGuestOrder(Item[] guestOrder){this.guestOrder = new ArrayList<>(Arrays.asList(guestOrder));}
+
+    public void addOrder(Item newOrder) {guestOrder.add(newOrder);}
+
+    public void removeOrder(int n) {guestOrder.remove(n);}
+
+    public void updatingOrder(int n,Item update){guestOrder.set(n,update);}
+
 }
