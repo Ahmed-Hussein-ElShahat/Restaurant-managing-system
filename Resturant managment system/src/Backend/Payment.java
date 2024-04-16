38141d1c5392a015534f6745bb707157094e3ec9
Hisham <heshamhatem08@gmail.com>
2024-04-16 21:52:11 +0200

    Payment class

--- Resturant managment system/src/Backend/Payment.java
+++ Resturant managment system/src/Backend/Payment.java
@@ -1,5 +1,36 @@
 package Backend;
 
 public abstract class Payment {
+    String method;
+    double amount;
+
+    public Payment() {
+    }
+    public Payment(String method, double amount) {
+        this.method = method;
+        this.setAmount(amount);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) throws IllegalArgumentException {
+        if ( amount >= 0 ) {
+            this.amount = amount;
+        }
+        else {
+            throw new IllegalArgumentException ("the amount can't be negative ");
+        }
+    }
+
+    public void print(){
+        System.out.println(this.getMethod() + " " + this.getAmount()) ;
+    }
     
 }
