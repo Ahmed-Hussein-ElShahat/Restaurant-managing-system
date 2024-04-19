12ee288f9d58a6bd38bdc64c2abd23bb5b0c252e
ShadeMaker <abdelrahman7987@gmail.com>
2024-04-19 14:33:52 +0200

    Adding Changes

--- Resturant managment system/src/Backend/Items/Appetizer.java
+++ Resturant managment system/src/Backend/Items/Appetizer.java
@@ -1,7 +1,28 @@
-package Backend.Items;
-
 import Backend.Item;
 
-public class Appetizer extends Item {
-    
+public class Appetizer {
+    private double soup = 0;
+    private double salad = 0;
+    private double garlicBread = 0;
+
+    public void setSoup(double soup) {
+        this.soup = soup;
+    }
+
+    public void setSalad(double salad) {
+        this.salad = salad;
+    }
+
+    public void setGarlicBread(double garlicBread) {
+        this.garlicBread = garlicBread;
+    }
+
+    public double getItemPrice(String item) {
+        return switch (item) {
+            case "Soup" -> soup;
+            case "Salad" -> salad;
+            case "GarlicBread" -> garlicBread;
+            default -> 0;
+        };
+    }
 }
